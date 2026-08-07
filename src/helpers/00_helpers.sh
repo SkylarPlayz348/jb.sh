85ed5786fc1cbc2c18c48296b6c680c5ddbc1ade
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-08-07 06:44:00 +0100

    Bump jb.sh version

--- src/helpers/00_helpers.sh
+++ src/helpers/00_helpers.sh
@@ -3,7 +3,7 @@
 ###
 # Defines
 ###
-JB_SH_VERSION="v1.2.1"
+JB_SH_VERSION="v1.3.0"
 
 if [ ! -n "${JB_HEADER+x}" ] && [ -f "/var/local/jailbreak.txt" ]; then
     JB_HEADER=$(cat /var/local/jailbreak.txt)
